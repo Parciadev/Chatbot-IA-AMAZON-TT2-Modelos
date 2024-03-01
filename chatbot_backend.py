95ab1a9b4f509e9a8b1f14cc9037a5a5d91b3fb4
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-03-01 11:43:17 -0300

    ya obtenida respuesta del modelo
    Continuare avanzando con el proyecto

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,26 +1,19 @@
-# Importar OS, Bedrock, ConversationChain, ConversationBufferMemory Langchain Modules
-
 import os
 from langchain.llms.bedrock import Bedrock
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
-# Funcion para invocar el modelo - conexion al cliente de bedrock con el perfil, model_id & inference params-
-
-def demo_chatbot(imput_text):
+def demo_chatbot(input_text):
     demo_llm = Bedrock(
-        credentials_profile_name= 'default',
-        model_id='ai21.j2-ultra-v1', # Importante
+        credentials_profile_name='default',
+        model_id = 'meta.llama2-70b-chat-v1',
         model_kwargs= {
             "temperature": 0.9,
             "top_p": 0.5,
-            "max_gen_len": 512
+            "max_gen_len": 256
         }
     )
     #return demo_llm
-
-# Probando LLM con un modelo de prediccion
-    return demo_llm.invoke(imput_text)
-
-response = demo_chatbot('hola, cual es tu nombre?')
+    return demo_llm.invoke(input_text)
+response = demo_chatbot('Hola, cual es el clima de santiago de hoy')
 print(response)
